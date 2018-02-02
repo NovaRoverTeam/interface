b03583fb7b4bbd94a0f56154d33a40b7c2d05ce7
BoredinPBL <thomas.123@live.com.au>
2018-02-02 16:22:26 -0500

    Added a picture in picture window to display an arm webcam
    Currently you set the number the same way as the Ricoh, thinking of
    adding a toggle feature

--- Assets/Scripts/Camera/ThetaWebCamTexture.cs
+++ Assets/Scripts/Camera/ThetaWebCamTexture.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ThetaWebCamTexture : MonoBehaviour {
 
@@ -9,8 +10,13 @@ public class ThetaWebCamTexture : MonoBehaviour {
     public GameObject sphere2;
     public GameObject warning;
     private bool cameraExists;
-
     public WebCamTexture webcamTexture;
+
+    public int PIPCameraNumber;
+    public RawImage PIPScreen;
+    public WebCamTexture PIPwebcamTexture;
+    private bool PIPcameraExists;
+    public GameObject PIPwarning;
 	
 	void Start() 
 	{
@@ -26,14 +32,37 @@ public class ThetaWebCamTexture : MonoBehaviour {
             sphere2.GetComponent<Renderer>().material.mainTexture = webcamTexture;
 
             webcamTexture.Play();
-		}
+        }
         else
         {
-			Debug.Log("no camera");
+            Debug.Log("no camera");
             cameraExists = false;
             warning.SetActive(true);
         }
-	}
+
+
+        PIPCameraNumber = PlayerPrefs.GetInt("CameraPIPNumber");
+        Debug.Log("beep");
+        if (devices.Length > PIPCameraNumber)
+        {
+            PIPcameraExists = true;
+            PIPwarning.SetActive(false);
+            WebCamTexture PIPwebcamTexture = new WebCamTexture();
+            PIPScreen.texture = PIPwebcamTexture;
+            PIPScreen.material.mainTexture = PIPwebcamTexture;
+            PIPwebcamTexture.Play();
+            Debug.Log("webcam should be active now");
+
+            PIPwebcamTexture.Play();
+        }
+        else
+        {
+            Debug.Log("no camera");
+            PIPcameraExists = false;
+            PIPwarning.SetActive(true);
+        }
+    }
+
 
     public void BackBtn(string LoadTarget)
     {
@@ -45,7 +74,16 @@ public class ThetaWebCamTexture : MonoBehaviour {
             }
 
         }
-            SceneManager.LoadScene(LoadTarget);
+
+        if (PIPcameraExists == true)
+        {
+            if (PIPwebcamTexture.isPlaying)
+            {
+                PIPwebcamTexture.Stop();
+            }
+
+        }
+        SceneManager.LoadScene(LoadTarget);
         
     }
 
@@ -58,6 +96,14 @@ public class ThetaWebCamTexture : MonoBehaviour {
                 webcamTexture.Stop();
             }
         }
-            Application.Quit();
+
+        if (PIPcameraExists == true)
+        {
+            if (PIPwebcamTexture.isPlaying)
+            {
+                PIPwebcamTexture.Stop();
+            }
+        }
+        Application.Quit();
     }
 }
--- Assets/Scripts/Menu/SettingsManager.cs
+++ Assets/Scripts/Menu/SettingsManager.cs
@@ -6,16 +6,28 @@ using UnityEngine.UI;
 public class SettingsManager : MonoBehaviour {
     public InputField camNumberField;
     public InputField camAngleField;
+    public InputField camPIPField;
     int camInt;
+    int camPIPInt;
     int camAngle;
     int defaultcamNumber = 0;
     int defaultcamAngle = 0;
+    int defaultcamPIPNumber = 1;
 
     public void camNumberFieldTracker() {
 
         camInt = int.Parse(camNumberField.text);
         PlayerPrefs.SetInt("CameraNumber", camInt);
         Debug.Log("Camera Number set to " + PlayerPrefs.GetInt("CameraNumber").ToString());
+
+    }
+
+    public void camPIPNumberFieldTracker()
+    {
+
+        camPIPInt = int.Parse(camPIPField.text);
+        PlayerPrefs.SetInt("CameraPIPNumber", camPIPInt);
+        Debug.Log("PIP Webcam Number set to " + PlayerPrefs.GetInt("CameraPIPNumber").ToString());
 
     }
 
@@ -33,21 +45,33 @@ public class SettingsManager : MonoBehaviour {
         if (PlayerPrefs.HasKey("CameraNumber"))
             
         { camNumberField.text = PlayerPrefs.GetInt("CameraNumber").ToString();
-            Debug.Log("Camera Number set to " + PlayerPrefs.GetInt("CameraNumber").ToString());
+            //Debug.Log("Camera Number set to " + PlayerPrefs.GetInt("CameraNumber").ToString());
         }
         else
         { camNumberField.text = defaultcamNumber.ToString();
-            Debug.Log("Camera Number set to " + PlayerPrefs.GetInt("CameraNumber").ToString());
+            //Debug.Log("Camera Number set to " + PlayerPrefs.GetInt("CameraNumber").ToString());
+        }
+
+        if (PlayerPrefs.HasKey("CameraPIPNumber"))
+
+        {
+            camPIPField.text = PlayerPrefs.GetInt("CameraPIPNumber").ToString();
+            //Debug.Log("PIP Webcam Number set to " + PlayerPrefs.GetInt("CameraPIPNumber").ToString());
+        }
+        else
+        {
+            camPIPField.text = defaultcamPIPNumber.ToString();
+            //Debug.Log("PIP Webcam Number set to " + PlayerPrefs.GetInt("CameraPIPNumber").ToString());
         }
 
 
         if (PlayerPrefs.HasKey("CameraAngle"))
         { camAngleField.text = PlayerPrefs.GetInt("CameraAngle").ToString();
-            Debug.Log("Camera Angle set to " + PlayerPrefs.GetInt("CameraAngle").ToString());
+            //Debug.Log("Camera Angle set to " + PlayerPrefs.GetInt("CameraAngle").ToString());
         }
         else
         { camAngleField.text = defaultcamAngle.ToString();
-            Debug.Log("Camera Angle set to " + PlayerPrefs.GetInt("CameraAngle").ToString());
+            //Debug.Log("Camera Angle set to " + PlayerPrefs.GetInt("CameraAngle").ToString());
         }
     }
 
