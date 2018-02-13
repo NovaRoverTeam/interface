f2eee541e638f88503e84a56b60cf03b2485a60b
Chow Yin Chun <chestonchow2010@gmail.com>
2018-02-13 17:04:46 +1100

    Map and ERT Feature Added

---
+++ Assets/Scripts/UI/MapAPI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapAPI : MonoBehaviour {
+
+	public RawImage img;
+
+	string url;
+	string APIKey = "AIzaSyCmAjN1F0s1S9VuvKJXnBF-z0Sez9c5L38";
+	int mapWidth = 250;
+	int mapHeight = 250;
+
+	float lat, lon, latInput, lonInput;
+	int zoom;
+	bool ERT = false;
+
+	public enum mapType {roadmap, satellite, hybrid, terrain}
+	public mapType mapSelected;
+
+	//IEnumerator Map(float markerLat = lat, float markerLon = lon){
+	IEnumerator Map(){
+		while (true) {
+			lat = PlayerPrefs.GetFloat("lat", 0);
+			lon = PlayerPrefs.GetFloat("lon", 0);
+			latInput = PlayerPrefs.GetFloat("latInput", 0);
+			lonInput = PlayerPrefs.GetFloat("lonInput", 0);
+			zoom = PlayerPrefs.GetInt("zoom", 0);
+			ERT = (PlayerPrefs.GetInt("ERT", 0) == 1);
+
+			url = "https://maps.googleapis.com/maps/api/staticmap?key="+APIKey+"&center="+lat+","+lon+"&markers=color:red%7C"+lat+","+lon+"&size="+mapWidth+"x"+mapHeight+"&maptype="+mapSelected+"&zoom="+zoom+((latInput != 0 && lonInput != 0 && ERT)?"&markers=color:green%7C"+latInput+","+lonInput:"");
+			//Debug.Log(url);
+			WWW www = new WWW (url);
+			while (!www.isDone);
+
+			img.texture = www.texture;
+			img.SetNativeSize();
+			yield return new WaitForSeconds(2f);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		img = gameObject.GetComponent<RawImage> ();
+		StartCoroutine (Map());
+	}
+}
---
+++ Assets/Scripts/UI/mapUI.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mapUI : MonoBehaviour {
+
+	public GameObject ERT;
+	public GameObject ERTInit;
+	public GameObject miniMap;
+	public GameObject miniMapInit;
+
+	public float lat, lon;
+	public int zoom = 14;
+
+	public InputField latInputField;
+	public InputField lonInputField;
+	public float latInput;
+	public float lonInput;
+
+
+	void Start(){
+		ERT.SetActive(false);
+		miniMap.SetActive(false);
+		setLocation();
+		PlayerPrefs.SetInt("zoom", zoom);
+		PlayerPrefs.SetInt("ERT", 0);
+	}
+
+	public void ZoomIn () {
+		PlayerPrefs.SetInt("zoom", ++zoom);
+	}
+
+	public void ZoomOut () {
+		PlayerPrefs.SetInt("zoom", (zoom > 0) ? --zoom : zoom);
+	}
+
+	public void ShowERT (){
+		ERT.SetActive(true);
+		ERTInit.SetActive(false);
+		PlayerPrefs.SetInt("ERT", 1);
+
+	}
+	public void HideERT (){
+		ERT.SetActive(false);
+		ERTInit.SetActive(true);
+		PlayerPrefs.SetInt("ERT", 0);
+	}
+
+	public void ShowMap (){
+		miniMap.SetActive(true);
+		miniMapInit.SetActive(false);
+	}
+	public void HideMap (){
+		miniMap.SetActive(false);
+		miniMapInit.SetActive(true);
+	}
+
+	void OnGUI() {
+        if (ERT.activeSelf && Event.current.Equals(Event.KeyboardEvent("return"))){
+        	latInput = float.Parse(latInputField.text);
+        	lonInput = float.Parse(lonInputField.text);
+
+        	PlayerPrefs.SetFloat("latInput", latInput);
+			PlayerPrefs.SetFloat("lonInput", lonInput);
+
+            print("Coordinate " + latInput+", "+ lonInput + " Submitted");
+        }
+    }
+
+    void setLocation(){
+    	lat = GPSRead.latitude;
+		lon = GPSRead.longitude;
+		PlayerPrefs.SetFloat("lat", lat);
+		PlayerPrefs.SetFloat("lon", lon);
+    }
+
+    void Update(){
+    	setLocation();
+    }
+}
