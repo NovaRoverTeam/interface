4687b86914df5532ffa1c8bd93d844beaf68b54a
BoredinPBL <thomas.123@live.com.au>
2018-04-29 16:19:04 +1000

    Integrated VRTK

---
+++ Assets/Scripts/Camera/FallBackWebcam.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FallBackWebcam : MonoBehaviour {
+
+    public int webCamNumber;
+    public int webcamOn;
+    private bool cameraExists = false;
+    public WebCamTexture webcamTexture;
+    public GameObject warning;
+    public RawImage fallBackScreen;
+
+    //Dropdown Objects for PIP cam
+    public Dropdown m_Dropdown_sidekick;
+    string m_MyString_sidekick;
+    int m_Index_sidekick;
+
+    //Drop down options:
+    //Use these for adding options to the Dropdown List
+    Dropdown.OptionData m_NewData;
+    //The list of messages for the Dropdown
+    List<Dropdown.OptionData> m_Messages = new List<Dropdown.OptionData>();
+
+    // Use this for initialization
+    void Start ()
+        {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        webcamOn = PlayerPrefs.GetInt("WebcamOn");
+            m_Dropdown_sidekick.ClearOptions();
+            //populate a message with all of the USB webcam devices
+            for (int i = 0; i < devices.Length; i++)
+            {
+                m_NewData = new Dropdown.OptionData();
+                m_NewData.text = devices[i].name;
+                //Debug.Log("Following added to Dropdown list " + m_NewData.text);
+                m_Messages.Add(m_NewData);
+            }
+
+            //Take each entry in the message List to generate both menus
+            foreach (Dropdown.OptionData message in m_Messages)
+            {
+                m_Dropdown_sidekick.options.Add(message);
+                m_Index_sidekick = m_Messages.Count - 1;
+            }
+            if (m_Dropdown_sidekick.value == 0)
+            { m_Dropdown_sidekick.captionText.text = devices[0].name; }
+
+
+            webCamNumber = PlayerPrefs.GetInt("CameraPIPNumber");
+            if (devices.Length > webCamNumber)
+            {
+                cameraExists = true;
+                warning.SetActive(false);
+                webcamTexture = new WebCamTexture(devices[webCamNumber].name);
+                fallBackScreen.texture = webcamTexture;
+                fallBackScreen.material.mainTexture = webcamTexture;
+                webcamTexture.Play();
+                Debug.Log("webcam should be active now");
+            }
+            else
+            {
+                Debug.Log("webcam not enabled");
+                cameraExists = false;
+                warning.SetActive(true);
+            }
+
+    }
+
+    public void BackBtn(string LoadTarget)
+    {
+        if (cameraExists == true)
+        {
+            if (webcamTexture.isPlaying)
+            {
+                webcamTexture.Stop();
+            }
+
+        }
+        SceneManager.LoadScene(LoadTarget);
+
+    }
+
+    public void ExitBtn()
+    {
+        if (cameraExists == true)
+        {
+            if (webcamTexture.isPlaying)
+            {
+                webcamTexture.Stop();
+            }
+        }
+        Application.Quit();
+    }
+
+    public void DropdownChange()
+    {
+        if (cameraExists == true)
+        {
+            if (webcamTexture.isPlaying)
+            {
+                webcamTexture.Stop();
+            }
+
+        }
+        webCamNumber = m_Dropdown_sidekick.value;
+        PlayerPrefs.SetInt("CameraPIPNumber", webCamNumber);
+        Debug.Log("Webcam set to " + webCamNumber.ToString());
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (m_Dropdown_sidekick.value == 0)
+        { m_Dropdown_sidekick.captionText.text = devices[0].name; }
+
+        webcamTexture = new WebCamTexture(devices[webCamNumber].name);
+        webcamTexture.Play();
+        Debug.Log("webcam should be active now");
+
+    }
+
+}
---
+++ Assets/Shaders/ExperimentalThetaSphere2.shader
@@ -0,0 +1,42 @@
+﻿Shader "Theta/ExperimentalSphere2" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_AlphaBlendTex ("Alpha Blend (RGBA)", 2D) = "white" {}
+		_OffsetU ("Offset U", Range(-0.5, 0.5)) = 0 
+		_OffsetV ("Offset V", Range(-0.5, 0.5)) = 0
+		_ScaleU ("Scale U", Range(0.8, 1.2)) = 1
+		_ScaleV ("Scale V", Range(0.8, 1.2)) = 1
+		_ScaleCenterU ("Scale Center U", Range(0.0, 1.0)) = 0 
+		_ScaleCenterV ("Scale Center V", Range(0.0, 1.0)) = 0
+	}
+	SubShader {
+		Tags { "RenderType" = "Opaque" "Queue" = "Background+1" }
+		
+		Cull Off
+
+		
+        CGPROGRAM
+
+        #pragma surface surf Lambert vertex:vert
+		sampler2D _MainTex;
+
+		 struct Input {
+            float2 uv_MainTex;
+            float4 color : COLOR;
+		};
+
+		void vert(inout appdata_full v) {
+			v.normal.xyz = v.normal * -1;
+		}
+
+		 void surf (Input IN, inout SurfaceOutput o) {
+             fixed3 result = tex2D(_MainTex, IN.uv_MainTex);
+             o.Albedo = result.rgb;
+             o.Alpha = 1;
+        }
+
+		ENDCG
+		
+		UsePass "Theta/ExperimentalSphere1/BASE"
+	}
+}
