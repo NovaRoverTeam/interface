e47a2f083bba741de36cf6eae0a49d88a31984eb
BoredinPBL <thomas.123@live.com.au>
2018-05-02 18:17:43 +1000

    Direction Compass Added
    Two compass objects designed and working. Currently demoing. Needs to be
    linked into ROS

---
+++ Assets/Scripts/UI/CompassManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassManager : MonoBehaviour {
+
+    private bool rotating = true;
+    public float compassBearingStart;
+    public float offset;
+    public float compassBearingNow;
+    public float compassPositionNow;
+    private float defaultCompassBearingStart;
+    public GameObject bearingCompass;
+    public GameObject destinationCompass;
+
+    
+	// Use this for initialization
+	void Start () {
+        compassBearingStart = defaultCompassBearingStart + offset;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        {
+            compassBearingNow = compassBearingNow + 0.01F;
+            compassPositionNow = defaultCompassBearingStart + compassBearingNow;
+            bearingCompass.transform.eulerAngles = new Vector3(-90, 0, compassPositionNow);
+            destinationCompass.transform.eulerAngles = new Vector3(-90, 0, compassPositionNow + offset);
+
+        }
+}
+
+    }
